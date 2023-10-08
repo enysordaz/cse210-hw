7e3cbaa9d2675a478403d3a28dbaa40ea793164c
enysordaz <ord23002@byui.edu>
2023-10-07 23:07:54 -0600

    Program 3 done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,31 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        // Do-while loop
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Guess Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Guess Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed correctly!");
+            }
+
+        }
     }
 }
