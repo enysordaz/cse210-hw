4a2a920304224ee36b2a589070ce2740af8b8995
enysordaz <ord23002@byui.edu>
2024-04-26 20:42:34 -0600

    Conditional statements learning activity a solution

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Promt the user for their grade percentage
+        Console.Write("What is your grade percentage? ");
+        string gradePercentage = Console.ReadLine();
+        int gradeLetter = int.Parse(gradePercentage);
+
+        //start the conditional statements
+        if (gradeLetter >= 90)
+        {
+            Console.WriteLine("Congratulations! You passed with an A! Amazing");
+        }
+        else if (gradeLetter >= 80)
+        {
+            Console.WriteLine("Congratulations! You passed with a B! You got this!");
+        }
+        else if (gradeLetter >= 70)
+        {
+            Console.WriteLine("You passed with a C! You can do it!");
+        }
+        else if (gradeLetter >= 60)
+        {
+            Console.WriteLine("You'll do better next time, you failed with a D");
+        }
+        else if (gradeLetter < 60)
+        {
+            Console.WriteLine("You can do better next time, you failed with a F");
+        }
+        // in case of wrong or no recognition of percentage
+        else
+        {
+            Console.WriteLine("I can't recognize the percentage grade");
+        }
     }
+    // end of program
 }
