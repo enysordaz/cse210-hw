e4c6043f02138dc5ab6111096fc64beb77f12551
enysordaz <ord23002@byui.edu>
2024-04-29 20:45:26 -0600

    Created new learning program for week 2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Creates new instance
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        // Calls function display from Job class for job1
+        //job1.Display();
+
+        // Creates a new Job instance
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        // Calls function display from Job class for job2
+        //job2.Display();
+
+        // Creates new instance for Resume
+        Resume myResume = new Resume();
+        myResume._name = "Enys Ordaz";
+        // Adds the information from the Job class to its class
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        // Displays the Resume
+        myResume.Display();
+
     }
 }
