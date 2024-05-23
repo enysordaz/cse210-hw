a5ac0faa50d501121874b6d1cae0010ee1184b2a
enysordaz <ord23002@byui.edu>
2024-05-23 17:25:23 -0600

    Learning activity

---
+++ prepare/Learning04/WritingAssignments.cs
@@ -0,0 +1,15 @@
+public class WritingAssignments : Assignment
+{
+    private string _title;
+
+    public WritingAssignments(string studentName, string topic, string title) : base(studentName, topic)
+    {
+        _title = title;
+    }
+
+    public string GetWritingInformation()
+    {
+        string studentName = GetStudentName();
+        return $"{_title} by {studentName}";
+    }
+}
